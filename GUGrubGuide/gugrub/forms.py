419841e67820a47f4cb79011c301e0193f7ff298
pinkypraew <pinkypraew9@gmail.com>
2015-03-23 18:44:15 +0000

    update add new review

--- GUGrubGuide/gugrub/forms.py
+++ GUGrubGuide/gugrub/forms.py
@@ -10,17 +10,22 @@ from django.forms.widgets import RadioSelect
 from django.forms.widgets import CheckboxSelectMultiple
 from django.forms.extras.widgets import SelectDateWidget
 
-class EateryReviewForm(forms.ModelForm):
 
-    CHOICES = (('1', '1'), ('2', '2'),('3', '3'),('4', '4'),('5', '5'))
+class EateryReviewForm(forms.ModelForm):
+    CHOICES = (('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'))
     date = forms.DateTimeInput
     title = forms.CharField(max_length=256, help_text="Please enter a title for your review.")
-    description = forms.CharField(widget = forms.Textarea, help_text="Please describe your experience.")
-    qualityRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect, help_text="How would you rate the quality of the food/drink?",)
-    valueRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect, help_text="How would you rate the value of the food/drink?", )
-    atmosphereRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect, help_text="How would you rate the atmosphere?", )
-    serviceRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect, help_text="How would you rate the service?", )
-    recommendRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect, help_text="How likely are you to recommend it to another student?", )
+    description = forms.CharField(widget=forms.Textarea, help_text="Please describe your experience.")
+    qualityRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect,
+                                      help_text="How would you rate the quality of the food/drink?", )
+    valueRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect,
+                                    help_text="How would you rate the value of the food/drink?", )
+    atmosphereRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect,
+                                         help_text="How would you rate the atmosphere?", )
+    serviceRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect,
+                                      help_text="How would you rate the service?", )
+    recommendRating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect,
+                                        help_text="How likely are you to recommend it to another student?", )
     picture = forms.FileField(label='Upload an Image', help_text="Would you like to upload an image?")
 
     def __init__(self, *args, **kwargs):
@@ -28,21 +33,21 @@ class EateryReviewForm(forms.ModelForm):
         super(EateryReviewForm, self).__init__(*args, **kwargs)
 
     class Meta:
-
         model = Review
-        fields = ('title', 'description', 'qualityRating', 'valueRating', 'atmosphereRating', 'serviceRating', 'recommendRating', 'picture')
-
+        fields = (
+            'title', 'description', 'qualityRating', 'valueRating', 'atmosphereRating', 'serviceRating', 'recommendRating',
+            'picture')
 
 
 class NewReviewForm(forms.ModelForm):
-
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super(NewReviewForm, self).__init__(*args, **kwargs)
 
     class Meta:
         model = Review
-        fields = ('eatery', 'title', 'description', 'qualityRating', 'valueRating', 'atmosphereRating', 'serviceRating', 'recommendRating', 'picture')
+        fields = ('eatery', 'title', 'description', 'qualityRating', 'valueRating', 'atmosphereRating', 'serviceRating',
+                  'recommendRating', 'picture')
         widgets = {
             'qualityRating': RadioSelect,
             'valueRating': RadioSelect,
